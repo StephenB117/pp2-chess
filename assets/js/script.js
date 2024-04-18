55501e334fa46f7538200e622f57782d95b23219
Stephen Bateson <stephen.lambkin@gmail.com>
2024-04-18 16:16:48 +0000

    feat:added subheading describing how to play the game

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const puzzleImage = document.getElementById("chess-image").innerHTML;
+const from = document.getElementById("from").innerText;
+const to = document.getElementById("to").innerText;
+
+
+function generatePuzzle() {
+
+
+}
+
+function checkAnswer() {
+
+    
+}
+
+function incrementScore() {
+
+    
+}
+
+function updateLeaderboard() {
+
+    
+}
+
+function resetGame() {
+
+    
+}
+

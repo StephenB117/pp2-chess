cabb78fc24786dbd775db7aee26c153d675df1fc
Stephen Bateson <stephen.lambkin@gmail.com>
2024-04-19 16:22:25 +0000

    added puzzle images and image folder to assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const puzzleImage = document.getElementById("chess-image").innerHTML;
 const from = document.getElementById("from").innerText;
 const to = document.getElementById("to").innerText;
-
+const playerTurn = document.getElementById("color-to-move");
 
 function generatePuzzle() {
 
